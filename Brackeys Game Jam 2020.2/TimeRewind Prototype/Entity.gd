3824fff1dff30c91d3a0969d966c45aa4a291558
Odaem <45592672+Odaem@users.noreply.github.com>
2020-08-03 00:06:53 +0200

    Simple Jump'n'Run mechanics

--- Brackeys Game Jam 2020.2/TimeRewind Prototype/Entity.gd
+++ Brackeys Game Jam 2020.2/TimeRewind Prototype/Entity.gd
@@ -1,11 +1,52 @@
 extends KinematicBody2D
 class_name Entity
 
+export var gravity = Vector2(0, 320)
+onready var prev_position = position
+var linear_velocity = Vector2()
+
+var move_direction = Vector2()
+export var movement_speed: float = 160.0
+
+var do_jump = false
+var may_jump = false
+export var jump_margin: float = 0.1
+export var jump_height: float = 64.0
+var jump_strength: float = 64.0
+var air_time: float = 0.0
+
 
 func _ready():
+	jump_strength = sqrt(2.0 * jump_height * gravity.length())
 	pass
+
+
+func _process(delta):
+	if move_direction.x != 0:
+		$Sprite.flip_h = move_direction.x < 0
 
 
 func _physics_process(delta):
+	var speed_multiplier
+	var speed_in_move_dir = linear_velocity.dot(move_direction.normalized())
+	if move_direction.length_squared() != 0:
+		speed_multiplier = min(max(movement_speed - speed_in_move_dir, 0), movement_speed)
+		linear_velocity += move_direction * speed_multiplier
+	else:
+		linear_velocity.x *= pow(0.001, delta)
 	
-	pass
+	if is_on_floor():
+		may_jump = true
+		air_time = 0.0
+	else:
+		air_time += delta
+		if air_time > jump_margin:
+			may_jump = false
+	
+	if do_jump and may_jump:
+		linear_velocity += Vector2(0, -1) * jump_strength - gravity * delta
+		may_jump = false
+	
+	linear_velocity += gravity * delta
+	
+	linear_velocity = move_and_slide(linear_velocity, Vector2(0,-1))
--- Brackeys Game Jam 2020.2/TimeRewind Prototype/Player.gd
+++ Brackeys Game Jam 2020.2/TimeRewind Prototype/Player.gd
@@ -1,5 +1,33 @@
 extends Entity
+class_name Player
 
+export(NodePath) var SpawnPosition
 
 func _ready():
-	pass
+	._ready()
+
+
+func _process(delta):
+	move_direction = Vector2()
+	
+	move_direction.x = -Input.get_action_strength("player_move_left") + Input.get_action_strength("player_move_right")
+	
+	if move_direction.length_squared() != 0:
+		move_direction = move_direction.normalized()
+	
+	if Input.is_action_pressed("player_jump"):
+		do_jump = true
+	else:
+		do_jump = false
+		
+	._process(delta)
+		
+#func _physics_process(delta):
+#	._physics_process(delta)
+
+
+func respawn():
+	var spawn_node = get_node(SpawnPosition)
+	if spawn_node:
+		position = spawn_node.position
+		linear_velocity = Vector2()
