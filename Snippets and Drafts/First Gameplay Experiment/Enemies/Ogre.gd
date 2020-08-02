790253186395aa518696ef6d152d39e39d78f7ee
Odaem <45592672+Odaem@users.noreply.github.com>
2020-08-02 17:45:56 +0200

    Improved Hits

--- Snippets and Drafts/First Gameplay Experiment/Enemies/Ogre.gd
+++ Snippets and Drafts/First Gameplay Experiment/Enemies/Ogre.gd
@@ -1,22 +1,43 @@
 extends KinematicBody2D
 
 export var target: NodePath
-export var speed: int = 20
+export var speed: float = 20
+export var max_health: int = 5
+var health: int
+var hit_timer := 0.0
+var got_hit := false
+var hit_normal := Vector2(0, 0)
+
 
 func _ready():
 	$AnimationTree.active = true
-
-func _process(delta):
-	var walk_dir := Vector2()
-	if target:
-		var t = get_node(target)
-		if t is Node2D:
-			walk_dir = (t.get_transform().get_origin() - position).normalized()
-			
-	if walk_dir != Vector2.ZERO:
-		move_and_slide(walk_dir * speed)
-		
+	health = max_health
 
 
-func get_hit():
-	queue_free()
+func _physics_process(delta):
+	if hit_timer > 0:
+		hit_timer -= delta
+	
+	if health <= 0:
+		queue_free()
+	
+	if got_hit:
+		move_and_slide(hit_normal * 10)
+		got_hit = false
+	else:
+		var walk_dir := Vector2()
+		if target:
+			var t = get_node(target)
+			if t is Node2D:
+				walk_dir = (t.get_transform().get_origin() - position).normalized()
+				
+		if walk_dir != Vector2.ZERO:
+			move_and_slide(walk_dir * speed)
+
+
+func get_hit(damage: int, dir_normal: Vector2 = Vector2(0, 0), hit_timeout: float = 0.2):
+	if hit_timer <= 0:
+		got_hit = true
+		health -= damage
+		hit_normal = dir_normal
+		print("got hit")
--- Snippets and Drafts/First Gameplay Experiment/Player/Sword.gd
+++ Snippets and Drafts/First Gameplay Experiment/Player/Sword.gd
@@ -4,6 +4,7 @@ extends RigidBody2D
 export var speed: float = 400
 export var rotation_speed: float = 2000
 export var move_point := Vector2(0, 0)
+export var attack: int = 3
 
 enum STATE {IDLE, DASHING}
 var state: int = STATE.IDLE
@@ -42,4 +43,5 @@ func _integrate_forces(phys_state):
 func _on_blade_hit(body: PhysicsBody2D):
 	if body.is_in_group("Enemies"):
 		if body.has_method("get_hit"):
-			body.get_hit()
+			print("hit")
+			body.get_hit(attack, (body.position - position).normalized())
