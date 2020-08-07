c19bebaabf7191c085db81e13b19ef5fd913f99d
Odaem <45592672+Odaem@users.noreply.github.com>
2020-08-07 15:43:29 +0200

    merging...

--- Brackeys Game Jam 2020.2/TimeRewind Prototype/new attempt/Player.gd
+++ Brackeys Game Jam 2020.2/TimeRewind Prototype/new attempt/Player.gd
@@ -43,3 +43,6 @@ func deactivate():
 func _on_body_shape_entered(body_id, body, body_shape, local_shape):
 	pass # Replace with function body.
 	
+
+func _on_body_shape_exited(body_id, body, body_shape, local_shape):
+	pass
