5d162fc0f45756470a906715b9d8babfae9b3f8e
nice42069 <nice42069@users.noreply.github.com>
2020-08-08 09:46:01 +0200

    Uploading Levels Two and Three for playtesting

--- Brackeys Game Jam 2020.2/TimeRewind Prototype/Rocket.gd
+++ Brackeys Game Jam 2020.2/TimeRewind Prototype/Rocket.gd
@@ -3,4 +3,15 @@ var dir = Vector2(-10,0)
 const SPEED = 50
 
 func _physics_process(delta):
-	move_and_slide(dir * SPEED)
+	var kbd2 = self.move_and_collide(dir * delta * SPEED)
+	var getType = self.get_meta("type") #prints rocket
+	var ray = $RayCast2D
+	if ray.is_colliding():
+		var collidedObject = ray.get_collider() #works
+#		print(collidedObject)
+		if collidedObject != null && "Rocket" in collidedObject.name:
+			print("collided with rocket")
+			self.queue_free()
+			collidedObject.queue_free()
+	if position.x > 90 && position.x < 130:
+		queue_free()
--- Brackeys Game Jam 2020.2/TimeRewind Prototype/RocketSpawner.gd
+++ Brackeys Game Jam 2020.2/TimeRewind Prototype/RocketSpawner.gd
@@ -3,13 +3,11 @@ extends Node2D
 var rocket  = preload("res://Rocket.tscn")
 
 func _on_respawnTimer_timeout():
-	var yLoc = rand_range(190,350)
+	var yLoc = rand_range(190,250)
 	var xLoc = 1600
 	var r = rocket.instance()
 	add_child(r)
+	r.set_meta("type", "rocket")
 	r.position = Vector2(xLoc,yLoc)
-	
-func _physics_process(delta):
-	pass
 	
 
